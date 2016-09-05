665c85f44bc0bde0666c31e354d2828945aab820
snowpool <snowpoollovely@gmail.com>
2016-09-06 00:07:48 +0900

    redirect error

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,14 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
   # providerとuidでUserレコードを取得する
   # 存在しない場合は、ブロック内のコードを実行して作成する
-  where(auth.slice(:provider, :uid)).first_or_create do |user|
+   where(auth.slice(:provider, :uid)).first_or_create do |user|
     # auth.provider には "twitter"、
     # auth.uidには twitterアカウントに基づいた個別のIDが入っている
     # first_or_createメソッドが自動でproviderとuidを設定してくれるので、
     # ここでは設定は必要ない
-    user.username = auth.info.nickname # twitterで利用している名前が入る
-    user.email = auth.info.email # twitterの場合入らない
-  end
+     user.username = auth.info.nickname # twitterで利用している名前が入る
+     user.email = auth.info.email # twitterの場合入らない
+    end
  end
 
  # Devise の RegistrationsController はリソースを生成する前に self.new_sith_session を呼ぶ
